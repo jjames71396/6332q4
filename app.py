fff1cce9e040e466de2a3ec07c7fb4af9bfdc0ba
unknown <jaj9608@mavs.uta.edu>
2023-06-26 16:29:09 -0500

    q4

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ app = Flask(__name__)
 
 counts = dict()
 data = None
+scatters = False
 
 @app.route('/')
 def index():
@@ -38,33 +39,74 @@ def vert():
     
 @app.route('/horiz', methods=['GET', 'POST'])
 def horiz():
+    global counts
+    global data
+    global scatters
+    scatters = False
+    arg = request.form.get('name')
+    print(arg)
+    args = arg.split(',')
+    if len(args) < 3 or len(args) >15:
+        return render_template('index.html')
+    counts = dict()
+    for i in args:
+      b = i.split(' ')
+      counts[b[0]] = counts.get(b[0], 0) + int(b[1])
+    data = pd.DataFrame(counts.items(), columns=['Letter', 'Value'])
+    
+    
+    total_values = data['Value'].sum()
+    data['Percentage'] = round((data['Value'] / total_values) * 100,2)
+    data = data.sort_values('Value')
     return render_template('horizontal.html')
     
 @app.route('/pie', methods=['GET', 'POST'])
 def pie():
     global counts
     global data
+    global scatters
+    scatters = False
     arg = request.form.get('name')
-    print(arg)
-    args = arg.split(' ')
-    if len(args) < 3 or len(args) >17:
+    args = arg.split(',')
+    if len(args) < 3 or len(args) >15:
         return render_template('index.html')
     counts = dict()
     for i in args:
-      counts[i] = counts.get(i, 0) + 1
+      b = i.split(' ')
+      counts[b[0]] = counts.get(b[0], 0) + int(b[1])
     data = pd.DataFrame(counts.items(), columns=['Letter', 'Value'])
     
     
     total_values = data['Value'].sum()
-    print(total_values)
     data['Percentage'] = round((data['Value'] / total_values) * 100,2)
-    print(data['Percentage'][0])
-    print(data.head())
     
     return render_template('pie.html')
     
 @app.route('/scatter', methods=['GET', 'POST'])
 def scatter():
+    global counts
+    global data
+    global scatters
+    scatters = True
+    arg = request.form.get('name')
+    print(arg)
+    args = arg.split(',')
+    if len(args) >10:
+        return render_template('index.html')
+    counts = dict()
+    data = pd.DataFrame()
+    
+    for j,i in enumerate(args):
+      b = i.split(' ')
+      if j == 0:
+          dict1 = {'x': int(b[0]),
+            'y': int(b[1]),
+            'c': int(b[2])
+          }
+          data = pd.DataFrame(dict1, index=[0])
+      else:
+        data.loc[len(data.index)] = [int(b[0]), int(b[1]), int(b[2])] 
+          
     return render_template('scatter.html')
 
 @app.route('/line', methods=['GET', 'POST'])
@@ -73,27 +115,22 @@ def line():
     
 @app.route('/data')
 def get_data():
-    # Load the data from a CSV file or any other source
-    #data = pd.read_csv("city.csv")
-    # Perform any necessary data preprocessing
+    global scatters
+    if not scatters:
+        pie_chart_data = [{'label': row['Letter'], 'value': row['Value'], 'percent': row['Percentage']} for _, row in data.iterrows()]
+        bar_chart_data = [{'label': row['Letter'], 'value': row['Value'], 'percent': row['Percentage']} for _, row in data.iterrows()]
+        chart_data = {
+            'bar_chart': bar_chart_data,
+            'pie_chart': pie_chart_data
+        }
+    else:
+        scatter_plot_data = []
+        for i, row in enumerate(data.iterrows()):
+            scatter_plot_data.append({'x': int(row[1]['x']), 'y': int(row[1]['y']), 'c': int(row[1]['c'])})
+        chart_data = {
+            'scatter_plot': scatter_plot_data
+        }
     
-    pie_chart_data = [{'label': row['Letter'], 'value': row['Value'], 'percent': row['Percentage']} for _, row in data.iterrows()]
-    bar_chart_data = [{'label': row['Letter'], 'value': row['Value'], 'percent': row['Percentage']} for _, row in data.iterrows()]
-
-    #state_populations = data.groupby('State')['Population'].sum().reset_index()
-    #state_populations = state_populations[state_populations['Population'] > 2500000]
-    #total_population = state_populations['Population'].sum()
-    #state_populations['Percentage'] = round((state_populations['Population'] / total_population) * 100,2)
-    # Convert the data to a format suitable for D3.js
-    #bar_chart_data = [{'label': row['State'], 'value': row['Population']} for _, row in state_populations.iterrows()]
-    #pie_chart_data = [{'label': row['State'], 'value': row['Percentage']} for _, row in state_populations.iterrows()]
-    #scatter_plot_data = [{'x': i+1, 'y': row[1]['Population']} for i, row in enumerate(state_populations.iterrows())]
-    # Prepare the data to be sent as JSON
-    chart_data = {
-        'bar_chart': bar_chart_data,
-        'pie_chart': pie_chart_data,
-        #'scatter_plot': scatter_plot_data
-    }
 
     return jsonify(chart_data)
 
